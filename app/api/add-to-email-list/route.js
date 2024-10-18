84d17597e13d05f18302521679cc4b2ba9cb920c
nazaruys <nazary.box@gmail.com>
2024-10-18 17:33:53 +0200

    Fix

--- app/api/add-to-email-list/route.js
+++ app/api/add-to-email-list/route.js
@@ -1,3 +1,4 @@
+"use client";
 import { NextResponse } from 'next/server';
 import { db } from '../../../firebase'; // Adjust based on your actual path
 import { collection, addDoc, query, where, getDocs } from "firebase/firestore";
